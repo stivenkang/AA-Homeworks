4a0cf3b78d6cef3e19759b71dd22dbdb4dcc25d5
Stiven Kang <kdaegyung@gmail.com>
2022-12-02 00:27:02 -0500

    octopus

---
+++ W4D5/octopus_problem.rb
@@ -0,0 +1,10 @@
+['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+#    => "fiiiissshhhhhh"
+
+def sluggish_octopus(fishes)
+    fishes.each_with_index do |fish1, i1|
+        fishes.each_with_index do |fish2, i2|
+            
+        end
+    end
+end
