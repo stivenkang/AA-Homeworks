0fd021fecc2b239ff79a6317c7bb3bed5185922d
Stiven Kang <kdaegyung@gmail.com>
2022-12-03 23:59:53 -0500

    W5D1 HW

---
+++ W5D1/lru_cache.rb
@@ -0,0 +1,28 @@
+class LRUCache
+    attr_reader :size, :cache
+    def initialize(size)
+        @size = size
+        @cache = []
+    end
+
+    def count
+        cache.count
+    end
+
+    def add(el)
+        if cache.include?(el)
+            cache.delete(el)
+            cache << el
+        elsif count >= size
+            cache.shift
+            cache << el
+        else
+            cache << el
+        end
+    end
+
+    def show
+        p @cache
+    end
+
+end
